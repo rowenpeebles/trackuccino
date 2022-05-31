e294a80341585fcfc56cc1da64d9d5090609f422
Rowen Peebles <rowen.peebles@outlook.com>
2022-05-31 20:53:56 +0100

    Initial Commit

---
+++ Trackuccino/TrackuccinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackuccinoApp.swift
+//  Trackuccino
+//
+//  Created by Rowen Peebles on 31/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TrackuccinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
